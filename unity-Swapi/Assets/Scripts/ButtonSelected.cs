ab2068dc596acb273ed236ff05ddef89a447829b
AndresGS <andresnn21@gmail.com>
2023-12-10 17:02:40 +0100

    buttons header fix

--- unity-Swapi/Assets/Scripts/ButtonSelected.cs
+++ unity-Swapi/Assets/Scripts/ButtonSelected.cs
@@ -6,17 +6,18 @@ using UnityEngine.UI;
 public class ButtonSelected : MonoBehaviour
 {
 	private Image image;
-
+	private RectTransform rectTransform;
 	private void Awake()
 	{
 		image= gameObject.GetComponent<Image>();
 		image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
+		rectTransform= gameObject.GetComponent<RectTransform>();
 	}
 
 	public void SetParentButton(Transform target)
 	{
 		image.color = new Color(image.color.r, image.color.g, image.color.b, 1);
-
+		rectTransform.pivot = target.GetComponent<RectTransform>().pivot;
 		transform.position = target.position;
 	}
 }
